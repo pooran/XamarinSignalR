ab4d957c84a130534bc89589e5f05ca2744923fc
Pooran Prasad Rajanna <pooran@whatelse.io>
2017-09-23 13:30:51 +0530

    initial commit

---
+++ XamarinSignalR.Server/Hubs/GroupHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace XamarinSignalR.Server.Hubs
+{
+    public class GroupHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+        public async Task JoinGroupAsync(string groupName)
+        {
+            await Groups.Add(Context.ConnectionId, groupName);
+            Clients.Group(groupName).addChatMessage(Context.User.Identity.Name + " joined.");
+        }
+        public Task LeaveGroup(string groupName)
+        {
+            return Groups.Remove(Context.ConnectionId, groupName);
+        }
+
+        public void SendMessage(MessageModel messageModel)
+        {
+            Clients.Group(messageModel.GroupId).GetCall(messageModel);
+        }
+    }
+}
---
+++ XamarinSignalR/XamarinSignalR/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinSignalR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendMessage_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJoinGroup_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSendGroupMessage_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLeaveGroup_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
